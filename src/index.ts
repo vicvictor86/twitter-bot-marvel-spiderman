915dfff3a9941f4ea02a58ebb1de23c82290b6df
Victor Emmanuel <vicbem@hotmail.com>
2022-07-20 13:24:13 -0300

    Bot can tweet the days to release the game everyday on 12pm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { TwitterApi } from 'twitter-api-v2';
+import { differenceInDays } from 'date-fns';
 
 function userLogin() {
     const userClient = new TwitterApi({
@@ -13,13 +14,27 @@ function userLogin() {
 }
 
 async function tweet() {
-    const { data: createdTweet } = await clientV2.tweet('Hello World3', {
-        poll: { duration_minutes: 120, options: ['Absolutely', 'For sure!'] },
+    const releaseDay = new Date(2022, 7, 12);
+    const daysToRelease = differenceInDays(releaseDay, new Date());
+    
+    const dayInMinutes = 1440;
+    const { data: createdTweet } = await clientV2.tweet(`Faltam ${daysToRelease} dias para o lançamento de Marvel's Spider-Man Remastered!`, {
+        poll: { duration_minutes: dayInMinutes, options: ['Meu deus que tristeza', 'Meu deus que felicidade'] },
     });
+
     console.log('Tweet', createdTweet.id, ':', createdTweet.text);
 }
 
 const clientV2 = userLogin();
 const now = new Date();
-setTimeout(tweet, new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 25, 0, 0).getTime() - Date.now());
-setInterval(tweet, 10000);
+
+let millisToNoon = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0, 0, 0).getTime() - Date.now();
+const oneDayInMili = 86400000;
+if (millisToNoon < 0) {
+    millisToNoon += oneDayInMili;
+}
+
+setTimeout(() => {
+    tweet();
+    setInterval(tweet, oneDayInMili);
+}, millisToNoon);
